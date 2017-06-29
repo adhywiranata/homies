043c59a234e4cf250667f428aee2c1f8ef50684b
Adhy <adhywiranatap@gmail.com>
2017-06-29 21:07:30 +0700

    update test spec and dependencies

--- src/index.ios.spec.tsx
+++ src/index.ios.spec.tsx
@@ -1,12 +1,23 @@
+import { shallow } from 'enzyme';
 import React from 'react';
 import 'react-native';
-import renderer from 'react-test-renderer';
 
 import Index from './index.android';
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />,
-  );
-  expect(tree).toBeDefined();
+  const wrapper = shallow(<Index />);
+  expect(wrapper).toBeDefined();
 });
+
+// import React from 'react';
+// import 'react-native';
+// import renderer from 'react-test-renderer';
+//
+// import Index from './index.android';
+//
+// it('renders correctly', () => {
+//   const tree = renderer.create(
+//     <Index />,
+//   );
+//   expect(tree).toBeDefined();
+// });
