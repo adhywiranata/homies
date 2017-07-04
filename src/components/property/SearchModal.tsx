9d87a54bae30bf52fadf2a5b159df742f0292ec7
Adhy <adhywiranatap@gmail.com>
2017-07-04 23:51:41 +0700

    update search modal styling

--- src/components/property/SearchModal.tsx
+++ src/components/property/SearchModal.tsx
@@ -6,17 +6,23 @@ import { HeaderWrapper } from '../core/HeaderCore';
 
 export default ({ toggleSearchModalVisibility }: { toggleSearchModalVisibility: any }) => (
   <View style={{ flex: 1 }}>
-    <HeaderWrapper style={{ elevation: 1, justifyContent: 'flex-start' }}>
+    <HeaderWrapper style={{ elevation: 1, justifyContent: 'space-between' }}>
       <TouchableOpacity onPress={toggleSearchModalVisibility} style={{ width: 30, padding: 10, paddingLeft: 0 }}>
         <Icon name={'chevron-left'} size={20} style={{ color: 'white', alignSelf: 'flex-end' }} />
       </TouchableOpacity>
+      <Icon name={'search'} size={15} style={{ color: '#DDD' }} />
       <TextInput
         autoFocus={true}
-        placeholder={'Search for your dream homes'}
+        placeholder={'Search for your dream homes...'}
+        selectionColor={'#DDDDDD'}
         placeholderTextColor={'#F5F5F5'}
         underlineColorAndroid={'transparent'}
         returnKeyType={'search'}
-        style={{ width: '88%', color: 'white', padding: 10 }} />
+        style={{ width: '70%', color: 'white', padding: 10, fontSize: 16 }}
+      />
+      <TouchableOpacity onPress={toggleSearchModalVisibility} style={{ width: 30, padding: 10, paddingLeft: 0 }}>
+        <Icon name={'map-marker'} size={20} style={{ color: 'white', alignSelf: 'flex-end' }} />
+      </TouchableOpacity>
     </HeaderWrapper>
     <View>
       <Text>Search Results</Text>
