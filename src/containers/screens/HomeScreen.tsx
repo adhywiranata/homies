c89a9c55fc047c5d3be07638dc9aa7b680417e66
Adhy <adhywiranatap@gmail.com>
2017-07-06 23:29:59 +0700

    add properties data fetching from home

--- src/containers/screens/HomeScreen.tsx
+++ src/containers/screens/HomeScreen.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import { FlatList, Modal, View } from 'react-native';
 
@@ -40,6 +41,37 @@ export default class HomeScreen extends React.Component<Props, State> {
     };
 
     this.toggleModalVisibility = this.toggleModalVisibility.bind(this);
+  }
+
+  componentDidMount() {
+    axios.post('https://us-central1-homies-3aa8b.cloudfunctions.net/api/graphql', {
+    query: `
+      query Properties {
+        properties {
+          id
+          name
+          description
+          category
+          price
+          facilities {
+            bedroom
+            bathroom
+            security
+          }
+          images {
+            url
+          }
+        }
+      }
+    `,
+  })
+  .then((response) => {
+    const properties = response.data.data.properties;
+    console.log(properties);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
   }
 
   toggleModalVisibility() {
