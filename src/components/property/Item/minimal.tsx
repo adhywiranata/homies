843ee03b931103c0ec2ad67194e885a69a153d36
Adhy <adhywiranatap@gmail.com>
2017-07-06 08:52:17 +0700

    add property Item minimal to history list

---
+++ src/components/property/Item/minimal.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Button from '../../core/Button';
+import Card from '../../core/Card';
+
+import {
+  ActionButtonWrapper,
+  InfoLabel,
+  InfoWrapper,
+  ItemInfo,
+  Price,
+  TitleLink,
+} from './core';
+
+export default ({ navigation }) => {
+  const handleItemDetail = () => { navigation.navigate('DetailsStack'); };
+  return (
+    <Card style={{ flexDirection: 'row' }}>
+      <View style={{ flex: 0.5 }}>
+        <Image
+          source={{ uri: 'https://d3p0bla3numw14.cloudfront.net/house/ho23/2383609/hos2383609-disewa-di-pamoyanan_bogor.jpg'}}
+          style={{ width: '100%', height: 80, resizeMode: 'cover' }}
+        />
+      </View>
+      <View style={{ flex: 1, padding: 5 }}>
+        <TitleLink
+          label={'Rumah Keren Pamoyanan Town House'}
+          handleItemDetail={handleItemDetail}
+        />
+        <Text style={{ paddingLeft: 5 }}>House, 100m square</Text>
+        <Price>Rp. 800.000.000</Price>
+      </View>
+    </Card>
+  );
+};
--- src/containers/screens/HistoryListScreen.tsx
+++ src/containers/screens/HistoryListScreen.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
+
+import PropertyItem from '../../components/property/Item/minimal';
 
 export interface Props { navigation: any; }
 // export interface State { }
+
+const historyData = [
+  {key: 'a'},
+  {key: 'b'},
+  {key: 'c'},
+  {key: 'd'},
+  {key: 'e'},
+  {key: 'f'},
+  {key: 'g'},
+  {key: 'h'},
+  {key: 'i'},
+  {key: 'j'},
+  {key: 'k'},
+  {key: 'l'},
+];
 
 export default class HistoryListScreen extends React.Component<Props, {}> {
   static navigationOptions = {
     drawerLabel: 'History',
   };
   render() {
+    const { navigation } = this.props;
+    const renderItem = ({item}) => <PropertyItem navigation={navigation} {...item} />;
     return (
-      <View>
-        <Text>
-          HISTORY LIST
-        </Text>
+      <View style={{ width: '100%', flexDirection: 'column' }}>
+        <FlatList
+          data={historyData}
+          renderItem={renderItem}
+          style={{ padding: 10, height: '85%' }}
+        />
       </View>
     );
   }
