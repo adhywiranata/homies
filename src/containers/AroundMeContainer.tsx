614ffe5e9ddfb7b895dd406c1feca9a60b66b77d
Adhy <adhywiranatap@gmail.com>
2017-07-08 21:47:18 +0700

    update mapview and marker

--- src/containers/AroundMeContainer.tsx
+++ src/containers/AroundMeContainer.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import MapView from 'react-native-maps';
 
 export interface Props { navigation: any; }
-export interface State { initialRegion: any; }
+export interface State { region: any; userMarker: any; markers: any }
 
 export default class extends React.Component<Props, State> {
   static navigationOptions = () => ({
@@ -13,39 +13,101 @@ export default class extends React.Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      initialRegion: {
-        latitude: 37.78825,
-        longitude: -122.4324,
+      region: {
+        latitude: -6.1754,
+        longitude: 106.8272,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      userMarker: {},
+      markers: [
+        {
+          key: 1,
+          latlng: {
+            latitude: -6.6244285,
+            longitude: 106.7967039,
+          },
+          title: 'wow',
+          description: 'ueah',
+        },
+        {
+          key: 2,
+          latlng: {
+            latitude: -6.6444285,
+            longitude: 106.7967039,
+          },
+          title: 'wow',
+          description: 'ueah',
+        },
+        {
+          key: 3,
+          latlng: {
+            latitude: -6.6244285,
+            longitude: 106.7987039,
+          },
+          title: 'wow',
+          description: 'ueah',
+        },
+      ],
     };
+
+    this.fetchLocation = this.fetchLocation.bind(this);
   }
 
   componentDidMount() {
+    this.fetchLocation();
+  }
+
+  fetchLocation() {
     navigator.geolocation.getCurrentPosition((position: any) => {
       this.setState({
-        initialRegion: {
-          longitude: position.longitude,
-          latitude: position.latitude,
-        }});
-  }, (error) => {
-      alert(JSON.stringify(error))
-  }, {
-      enableHighAccuracy: true,
-      timeout: 20000,
-      maximumAge: 1000
-  });
+        region: {
+          ...this.state.region,
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+        },
+        userMarker: {
+          latlng: {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude,
+          },
+        },
+      });
+    }, (error) => {
+        alert(JSON.stringify(error))
+    }, {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+    });
   }
 
   render() {
-    const { initialRegion } = this.state;
+    const { region, userMarker, markers } = this.state;
     return (
       <View style={{ width: '100%', flex: 1 }}>
         <MapView
           style={{ width: '100%', height: '100%' }}
-          initialRegion={initialRegion}
-        />
+          initialRegion={region}
+          region={region}
+        >
+          { userMarker.latlng && (
+            <MapView.Marker
+              coordinate={userMarker.latlng}
+              title={'me'}
+              description={'me'}
+              pinColor={'#9B59B6'}
+            />
+          )}
+          { markers.map(marker => (
+            <MapView.Marker
+              key={marker.key}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description}
+            />
+          ))}
+        </MapView>
       </View>
     );
   }
